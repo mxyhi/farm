00b51c35fd77d1779873eb9034b5efc469e980d6
shulandmimi <sshuang141@163.com>
2024-08-29 09:02:08 +0800

    feat: support esm and cjs module system generate

--- crates/compiler/tests/bundle.rs
+++ crates/compiler/tests/bundle.rs
@@ -1,15 +1,19 @@
 use std::{collections::HashMap, path::PathBuf};
 
-use farmfe_core::config::{bool_or_obj::BoolOrObj, config_regex::ConfigRegex, Mode, TargetEnv};
+use farmfe_core::config::{
+  bool_or_obj::BoolOrObj, config_regex::ConfigRegex,
+  partial_bundling::PartialBundlingEnforceResourceConfig, Mode, TargetEnv,
+};
 mod common;
 use crate::common::{
   assert_compiler_result_with_config, create_compiler_with_args, AssertCompilerResultConfig,
 };
+use farmfe_core::config::Config;
 
 #[allow(dead_code)]
 #[cfg(test)]
-fn test(file: String, crate_path: String) {
-  use common::get_config_field;
+fn test(file: String, crate_path: String, f: Option<impl Fn(&mut Config)>) {
+  use common::{format_output_name, get_config_field, get_dir_config_files};
   use farmfe_core::config::partial_bundling::PartialBundlingEnforceResourceConfig;
 
   use crate::common::try_read_config_from_json;
@@ -21,64 +25,98 @@ fn test(file: String, crate_path: String) {
 
   let entry_name = "index".to_string();
 
-  let config_entry = cwd.to_path_buf().join("config.json");
+  let files = get_dir_config_files(cwd);
   let runtime_entry = cwd.to_path_buf().join("runtime.ts");
 
-  let config_from_file = try_read_config_from_json(config_entry);
+  for (name, config_entry) in files {
+    // let config_entry = cwd.to_path_buf().join("config.json");
 
-  let compiler =
-    create_compiler_with_args(cwd.to_path_buf(), create_path_buf, |mut config, plugins| {
-      config.mode = Mode::Production;
+    let config_from_file = try_read_config_from_json(config_entry);
 
-      if runtime_entry.is_file() {
-        let runtime_entry = runtime_entry.to_string_lossy().to_string();
-        config.runtime.path = runtime_entry;
-      }
+    let compiler = create_compiler_with_args(
+      cwd.to_path_buf(),
+      create_path_buf.clone(),
+      |mut config, plugins| {
+        config.mode = Mode::Development;
 
-      config.input = HashMap::from_iter(vec![(entry_name.clone(), file)]);
+        if runtime_entry.is_file() {
+          let runtime_entry = runtime_entry.to_string_lossy().to_string();
+          config.runtime.path = runtime_entry;
+        }
 
-      config.minify = Box::new(BoolOrObj::Bool(false));
-      config.tree_shaking = Box::new(BoolOrObj::Bool(false));
+        config.input = HashMap::from_iter(vec![(entry_name.clone(), file.clone())]);
 
-      config.external = vec![ConfigRegex::new("(^node:.*)"), ConfigRegex::new("^fs$")];
-      config.output.target_env = TargetEnv::Node;
-      // config.output.format = ModuleFormat::CommonJs;
+        config.minify = Box::new(BoolOrObj::Bool(false));
+        config.tree_shaking = Box::new(BoolOrObj::Bool(false));
 
-      // TODO: multiple bundle
+        config.external = vec![ConfigRegex::new("(^node:.*)"), ConfigRegex::new("^fs$")];
+        config.output.target_env = TargetEnv::Custom("library-node".to_string());
+        // config.output.format = ModuleFormat::CommonJs;
+
+        // TODO: multiple bundle
+        config.partial_bundling.enforce_resources = vec![PartialBundlingEnforceResourceConfig {
+          test: vec![ConfigRegex::new(".+")],
+          name: "index".to_string(),
+        }];
+
+        if let Some(config_from_file) = config_from_file {
+          if let Some(mode) = get_config_field(&config_from_file, &["mode"]) {
+            config.mode = mode;
+          }
+
+          if let Some(format) = get_config_field(&config_from_file, &["output", "format"]) {
+            config.output.format = format;
+          }
+
+          if let Some(target_env) = get_config_field(&config_from_file, &["output", "targetEnv"]) {
+            config.output.target_env = target_env;
+          }
+        }
+
+        if let Some(f) = f.as_ref() {
+          f(&mut config);
+        }
+
+        (config, plugins)
+      },
+    );
+
+    compiler.compile().unwrap();
+
+    assert_compiler_result_with_config(
+      &compiler,
+      AssertCompilerResultConfig {
+        entry_name: Some(entry_name.clone()),
+        output_file: Some(format_output_name(name)),
+        ignore_emitted_field: false,
+        ..Default::default()
+      },
+    );
+  }
+}
+
+fn single_bundle_test(file: String, crate_path: String) {
+  test(file, crate_path, None::<fn(&mut Config)>);
+}
+
+fn multiple_bundle_test(file: String, crate_path: String) {
+  test(
+    file,
+    crate_path,
+    Some(|config: &mut Config| {
       config.partial_bundling.enforce_resources = vec![PartialBundlingEnforceResourceConfig {
-        test: vec![ConfigRegex::new(".+")],
-        name: "index".to_string(),
+        name: "bundle1".to_string(),
+        test: vec![ConfigRegex::new("^bundle2.+")],
       }];
-
-      if let Some(config_from_file) = config_from_file {
-        if let Some(mode) = get_config_field(&config_from_file, &["mode"]) {
-          config.mode = mode;
-        }
-
-        if let Some(format) = get_config_field(&config_from_file, &["output", "format"]) {
-          config.output.format = format;
-        }
-
-        if let Some(target_env) = get_config_field(&config_from_file, &["output", "targetEnv"]) {
-          config.output.target_env = target_env;
-        }
-      }
-
-      (config, plugins)
-    });
-
-  compiler.compile().unwrap();
-
-  assert_compiler_result_with_config(
-    &compiler,
-    AssertCompilerResultConfig {
-      entry_name: Some(entry_name),
-      ignore_emitted_field: false,
-      ..Default::default()
-    },
+    }),
   );
 }
 
-farmfe_testing::testing! {"tests/fixtures/bundle/library/**/index.ts", test}
+// farmfe_testing::testing! {"tests/fixtures/bundle/library/reexport/reexport_hybrid_cjs/namespace/**/index.ts", single_bundle_test}
+
+farmfe_testing::testing! {
+  "tests/fixtures/bundle/multiple_bundle/reexport/reexport_cross_bundle_1/**/index.ts",
+  multiple_bundle_test
+}
 // farmfe_testing::testing! {"tests/fixtures/runtime/bundle/cjs/export/entryExportStar/**/index.ts", test}
 // farmfe_testing::testing! {"tests/fixtures/runtime/bundle/external/import/namespace/**/index.ts", test}
