6e591e30484727cd3554137b034c0ed323191349
苏向夜 <fu050409@163.com>
2024-09-14 13:01:29 +0800

    refactor(preview): export preview server

--- packages/core/src/server/preview.ts
+++ packages/core/src/server/preview.ts
@@ -1,1 +1,13 @@
-export {};
+import { UserConfig, UserPreviewServerConfig } from '../config/types.js';
+import { httpServer as HttpServer } from './http.js';
+
+class PreviewServer extends HttpServer {
+  previewOptions: UserPreviewServerConfig;
+
+  constructor(readonly inlineConfig: UserConfig) {
+    super();
+    this.previewOptions = inlineConfig.preview;
+  }
+}
+
+export { PreviewServer };
